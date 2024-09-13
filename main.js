86e367f9515a2b8acdf7a8be24620f8384d713df
aizeeen <ihebbacha2006@gmail.com>
2024-09-13 18:55:38 +0000

    noth

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // document.getElementById("count-el").innerText = 5;
 
-let count = 5
-count = 3 
-count = 1
-console.log(count)
+// initialize the count as 0
+// listen for clicks on the increment button 
+// increment the count variable when the button is clicked 
+// change the count-el to reflect the new count 
+
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+console.log(countEl)
+
+
+
+function increment(){
+    // for testing ,, console.log(" clicked")
+    count = count +1 
+    countEl.innerText = count 
+    
+}
